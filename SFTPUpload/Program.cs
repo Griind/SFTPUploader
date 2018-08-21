70d7d2b3262f11cca05a59445c61da868333fa28
Roman Svitukha <griind@gmail.com>
2018-08-20 21:40:44 -0500

    Add project files.

---
+++ SFTPUpload/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFTPUpload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var info = new SftpInfo()
+            {
+                Sourcefile = @"C:\someFIle.csv",
+                Destination = @"./",
+                Host = "192.168.10.185",
+                UserName = "username",
+                Password = "password",
+                Port = 22
+            };
+              Sftp.UploadSFTPFile(info);
+        }
+        public class SftpInfo
+        {
+            public string Sourcefile { get; set; }
+            public string Destination { get; set; }
+            public string Host { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public int Port { get; set; }
+        }
+    }
+}
---
+++ SFTPUpload/sftp.cs
@@ -0,0 +1,34 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SFTPUpload.Program;
+
+
+class Sftp
+{
+    public static void UploadSFTPFile(SftpInfo info)
+    {
+        try
+        {
+            using (SftpClient client = new SftpClient(info.Host, info.Port, info.UserName, info.Password))
+            {
+                client.Connect();
+                client.ChangeDirectory(info.Destination);
+                using (FileStream fs = new FileStream(info.Sourcefile, FileMode.Open))
+                {
+                    client.BufferSize = 4 * 1024;
+                    client.UploadFile(fs, Path.GetFileName(info.Sourcefile));
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An Error has Occured: " + ex);
+        }
+    }
+}
+
